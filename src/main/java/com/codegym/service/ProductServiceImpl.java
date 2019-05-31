dcd012f0ac6e041d00b88acdc5af4dbbceef89bc
minhduc <minhduc1994vcu@gmail.com>
2019-05-31 15:05:30 +0700

    add class serviceImpl

---
+++ src/main/java/com/codegym/service/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+package com.codegym.service;
+
+import com.codegym.model.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductServiceImpl implements ProductService {
+    private static Map<Integer, Product> products;
+    static {
+        products = new HashMap<>();
+        products.put(1, new Product(1, "samsung s10", 1500, "luxuri", "samsung"));
+        products.put(2, new Product(2, "asus m1", 600, "cheap", "asus"));
+        products.put(3, new Product(3, "iphone 7", 800, "normal", "iphone"));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+
+    @Override
+    public void save(Product product) {
+        products.put(product.getId(),product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return products.get(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        products.put(id, product);
+    }
+
+    @Override
+    public void remove(int id) {
+        products.remove(id);
+    }
+}
